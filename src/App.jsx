100bdf50778c8cf8bc488a552bc84c9ba0b45021
Sayem Molla <sayemmolla111@outlook.com>
2023-03-05 21:22:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+      <h1 className='bg-red-500 text-white p-5'>hello word</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/reducer/incDec.js
@@ -0,0 +1,12 @@
+const initialState = 0;
+
+const updateNumber = (state = initialState, action) = {
+    switch(key) {
+    case: "INCREMENT":
+    return state + 1;
+    break;
+
+    default:
+        break;
+}
+}
